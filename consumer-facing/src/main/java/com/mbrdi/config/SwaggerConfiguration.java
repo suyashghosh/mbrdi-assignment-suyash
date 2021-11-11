2535172cd98f0978d8375c28027f0a129b09c375
Suyash Ghosh <suyashghosh@gmail.com>
2021-11-11 12:32:13 +0530

    swagger base package

--- consumer-facing/src/main/java/com/mbrdi/config/SwaggerConfiguration.java
+++ consumer-facing/src/main/java/com/mbrdi/config/SwaggerConfiguration.java
@@ -17,7 +17,7 @@ public class SwaggerConfiguration {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.any())              
+          .apis(RequestHandlerSelectors.basePackage("com.mbrdi"))              
           .paths(PathSelectors.any())                          
           .build();                                           
     }
